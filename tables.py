4a925faa70df6897d03172340cef39c413022e33
Dario Passarello <dario.passarello@gmail.com>
2020-06-10 21:05:17 +0200

    2D

--- tables.py
+++ tables.py
@@ -1,15 +1,12 @@
 from collections import namedtuple
 import json
-import sympy as sym
-
-sym.init_printing()
-
-x = sym.symbols('x')
+from functools import lru_cache
 
 EPS = 0.00001
 field = namedtuple("field", ["id", "name", "description", "unit", "decimals"])
 
 
+@lru_cache
 def load_fields_from_json(filename: str):
     with open(filename, "r") as j_file:
         j_string = j_file.read()
@@ -17,7 +14,7 @@ def load_fields_from_json(filename: str):
         return dict(map(lambda item: (item["id"], field(**item)), j_array))
 
 
-def ordered_search(array, item, key):
+def ordered_search(array, item, key=lambda x: x):
     hi_idx = None
     for i, row in enumerate(array):
         if float_equals(key(row), item):
@@ -32,27 +29,20 @@ def ordered_search(array, item, key):
     return False, (low_row, hi_row)
 
 
-def calculate_quality(low_row, hi_row, value, key):
+def calculate_quality(low_row, hi_row, value, key=lambda x: x):
     low_value = min(key(low_row), key(hi_row))
     hi_value = max(key(low_row), key(hi_row))
-
-    if not(low_value < value < hi_value):
-        print("LOW:%f CURR:%f HI:%f" % (low_value,value,hi_value))
+    if not (low_value < value < hi_value):
+        print("LOW:%f CURR:%f HI:%f" % (low_value, value, hi_value))
         raise ValueError("Interpolation out of range")
     qlt = (value - low_value) / (hi_value - low_value)
-
-    print(f'x = ({value} - {low_value}) / ({(hi_value)} - {(low_value)})')
-
     return qlt
 
 
 def interpolate_rows(low_row, hi_row, quality):
     mid_row = dict()
     for col in low_row.keys():
-        print(f'{col} = {low_row[col]} * (1 - {quality}) + {hi_row[col]} * {quality}', end='\n\n')
-
         mid_row[col] = low_row[col] * (1 - quality) + hi_row[col] * quality
-
     return mid_row
 
 
